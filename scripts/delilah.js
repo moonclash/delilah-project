8348d72ecc2c8b93c26053674669916a769a1066
moonclash <emladenoff@gmail.com>
2018-01-29 22:20:28 +0000

    start the work on some JS

--- scripts/delilah.js
+++ scripts/delilah.js
@@ -1,3 +1,21 @@
-const delilah = {
-	
-}
+(function iife() {
+	document.addEventListener('DOMContentLoaded', () => {
+		const Element = {
+			init(element) {
+				this.element = document.querySelector(element);
+				return this;
+			},
+			find(child) {
+				const childd = this.element.querySelector(child);
+				console.log(childd);
+				return childd;
+				
+			}
+		}
+		const el = Object.create(Element);
+		el.init('header');
+		el.find('h1');
+	});
+})();
+
+// select element
