1d3cd40be3f41a839fb17818f8ff85e30310a42c
moonclash <emladenoff@gmail.com>
2018-01-31 07:55:03 +0000

    add more JS tweaks

--- scripts/delilah.js
+++ scripts/delilah.js
@@ -3,18 +3,37 @@
 		const Element = {
 			init (domElement) {
 				this.element = document.querySelector(domElement);
+				console.log(this);
 				return this;
 			}
 		}
 
 		Element.find = function(el) {
 			return this.element.querySelector(el);
+		};
+
+		Element.event = function(event, action) {
+			this.element.addEventListener(event, action);
+		};
+
+		const Elements = Object.create(Element);
+
+		Elements.init = function(domElements) {
+			this.element = document.querySelectorAll(domElements);
+			return this;
+		}
+
+		Elements.event = function(event, action) {
+			this.element.forEach(element => element.addEventListener(event, action));
 		}
 		
-		const el = Object.create(Element);
-		el.init('header');
-		el.find('h1').addEventListener('click', () => alert('hey'));
-		
+		const els = Object.create(Elements);
+		const lis = els.init('li');
+		function foo () {
+			console.log(this.textContent);
+		}
+		lis.event('click', foo);
+
 	});
 })();
 
